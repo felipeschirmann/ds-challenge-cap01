6171cdeeca316cb0ec546734838d2ce46811e31c
Felipe Schirmann <felipeschirmann@gmail.com>
2021-11-27 12:26:19 -0300

    Entity Client

---
+++ backend/src/test/java/com/ds/challenge/cap01/Cap01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.challenge.cap01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cap01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
