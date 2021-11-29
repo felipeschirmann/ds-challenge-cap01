584eddcdd3fe6a0610ab97e6f472f73dafbf0390
Felipe Schirmann <felipeschirmann@gmail.com>
2021-11-29 15:00:10 -0300

    exceptions

--- backend/src/main/java/com/ds/challenge/cap01/services/ClientService.java
+++ backend/src/main/java/com/ds/challenge/cap01/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.ds.challenge.cap01.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.ds.challenge.cap01.dto.ClientDTO;
 import com.ds.challenge.cap01.entities.Client;
 import com.ds.challenge.cap01.repositories.ClientRepository;
+import com.ds.challenge.cap01.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -19,6 +22,13 @@ public class ClientService {
 	public Page<ClientDTO> findAll(PageRequest pageRequest) {
 		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(x -> new ClientDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not Found"));
+		return new ClientDTO(entity);
 	}
 
 }
